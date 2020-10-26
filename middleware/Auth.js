86e7b758fea72177bd7ce39e9cd18ecf91ddde9f
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-26 11:08:07 +0545

    fixed 401 response on null token

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -2,8 +2,8 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const authHeader = req.header("Authorization");
-  const token = authHeader && authHeader.split(' ')[1]
-  if (!token) {
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token || token == null) {
     return res.status(401).json({ message: "Auth Error, no token is passed" });
   }
   try {
