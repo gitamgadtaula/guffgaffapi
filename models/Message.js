3043ffe6e90785f721400e512ce71ab574bd3c7b
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-27 15:07:57 +0545

    created room controller and message controller

--- models/Message.js
+++ models/Message.js
@@ -4,6 +4,6 @@ const msgSchema = new mongoose.Schema({
   room: { ref: "Room" },
   user: { ref: "User" },
   msg: { type: String },
-  created_at: { default: Date.now },
+  created_at: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model("Message", msgSchema);
---
+++ routes/message.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+// importing UserController
+var UserController = require("../controllers/UserController");
+var Auth = require("../middleware/Auth");
