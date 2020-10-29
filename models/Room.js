3dade71b0d864d4051f12195b7f20428378cf1c6
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-29 16:02:58 +0545

    changed Room model

--- models/Room.js
+++ models/Room.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const roomSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   // topic: { type: String, default: "Some random topic" },
-  users: { type: Array, required: true },
+  users: { type: Array, required: true, unique: true },
   messasges: { ref: "Message", type: mongoose.Schema.Types.ObjectId },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
