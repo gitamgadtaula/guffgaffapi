b08f77d301f20f5c2dad6aa7abd7d7b44983b9bc
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-30 21:08:19 +0545

    chat controller done

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -1,16 +1,71 @@
-//create a user
-module.exports.create = async (req, res) => {
+const User = require("../models/User");
+const Room = require("../models/Room");
+const Chat = require("../models/Chat");
 
-  const room = new Room({
-    full_name: req.body.full_name,
-    email: req.body.email,
-    password: req.body.password,
-    username: req.body.username,
-  });
-  try {
-    const newUser = await user.save();
-    return res.status(200).json(newUser);
-  } catch (error) {
-    return res.json(error);
+//Post chat message
+module.exports.create = async (req, res) => {
+  // Check if message is empty
+  if (req.body.message == null) {
+    return res.status(400).json({
+      success: false,
+      msg: "Message cannot be empty",
+    });
   }
+  // Check if room already exists
+  Room.findOne(
+    {
+      $and: [
+        {
+          users: { $in: req.user.id },
+        },
+        {
+          users: { $in: req.body.recipient_id },
+        },
+      ],
+    },
+    (err, result) => {
+      if (err) throw err;
+      if (result != null) {
+        // If exists, save chat using that room _id
+        const newChat = new Chat({
+          sender: req.user.id,
+          room: result.id,
+          message: req.body.message,
+        });
+
+        newChat.save().then((message) => {
+          return res.status(201).json({
+            success: true,
+            msg: "Message saved",
+            value: message,
+          });
+        });
+      } else {
+        // Create new room
+        Room.create(
+          {
+            users: [req.user.id, req.body.recipient_id],
+          },
+          (err, result) => {
+            if (err) throw err;
+            Chat.create(
+              {
+                sender: req.user.id,
+                room: result.id,
+                message: req.body.message,
+              },
+              (err, message) => {
+                if (err) throw err;
+                res.status(201).json({
+                  success: true,
+                  msg: "Message saved",
+                  value: message,
+                });
+              }
+            );
+          }
+        );
+      }
+    }
+  );
 };
