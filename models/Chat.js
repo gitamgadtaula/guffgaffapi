02f81c12a93285d4184bd3fd102f9b055b38f236
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-30 21:08:01 +0545

    chat model finalised

--- models/Chat.js
+++ models/Chat.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const msgSchema = new mongoose.Schema({
-  room: { ref: "Room" },
-  user: { ref: "User" },
-  msg: { type: String },
+  room: { ref: "Room", type: mongoose.Schema.Types.ObjectId },
+  sender: { ref: "User", type: mongoose.Schema.Types.ObjectId },
+  message: { type: String },
   created_at: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model("Message", msgSchema);
