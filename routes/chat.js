87385ca23d3ef49a5b02a72e39299496f1de2963
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-31 19:48:14 +0545

    added findroom middleware

--- routes/chat.js
+++ routes/chat.js
@@ -5,8 +5,9 @@ var ChatController = require("../controllers/ChatController");
 
 var Auth = require("../middleware/Auth");
 var Room = require("../middleware/Room");
+var FindRoom = require("../middleware/FindRoom");
 
 router.post("/create", Auth, Room, ChatController.create);
-router.get("/getall", Auth, ChatController.getAll);
+router.get("/getall", Auth, Room, FindRoom, ChatController.getAll);
 
 module.exports = router;
