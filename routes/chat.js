cf1d36586de54eed0f2fa96d879a057475f49560
gitamgdtaula <gitamgadtaula@gmail.com>
2020-10-31 21:11:58 +0545

    changed get request to post in msg getall

--- routes/chat.js
+++ routes/chat.js
@@ -8,6 +8,6 @@ var Room = require("../middleware/Room");
 var FindRoom = require("../middleware/FindRoom");
 
 router.post("/create", Auth, Room, ChatController.create);
-router.get("/getall", Auth, Room, FindRoom, ChatController.getAll);
+router.post("/getall", Auth, Room, FindRoom, ChatController.getAll);
 
 module.exports = router;
