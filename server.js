d6158b9d0e0915e79cbad8ee052761cf6b677bd9
gitamgdtaula <gitamgadtaula@gmail.com>
2020-11-05 20:11:42 +0545

    Socket io

--- server.js
+++ server.js
@@ -45,23 +45,34 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to mongodb .."));
 
 // Socket connection
-var server = require("https").createServer(app);
-var io = require("socket.io").listen(server);
+var http = require("http").createServer(app);
+
+var io = require("socket.io")(http, {
+  handlePreflightRequest: function (req, res) {
+    var headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": req.headers.origin,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
 
 //Whenever someone connects this gets executed
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
   console.log("A user connected");
   //Send a message after a timeout of 4seconds
-  setTimeout(function () {
-    socket.send("Sent a message 4 seconds after connection!");
-  }, 4000);
-  socket.on("disconnect", function () {
+  // setTimeout(function () {
+  //   socket.send("Sent a message 4 seconds after connection!");
+  // }, 4000);
+  socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
 });
 
 //listen to the changes in the app
-server.listen(process.env.PORT, () =>
+http.listen(process.env.PORT, () =>
   console.log(`server running in http://localhost:${process.env.PORT}/`)
 );
 
